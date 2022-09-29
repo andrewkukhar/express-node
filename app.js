1bf06391e2777236695fca2132bc2ff582ebe31c
andreyrise <andrey.rise@gmail.com>
2022-09-28 20:16:50 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import { requestTime, logger } from './middleware.js'
+
+const __dirname = path.resolve()
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'ejs'))
+console.log(app.get('view engine'))
+
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(requestTime)
+app.use(logger)
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/download', (req, res) => {
+    console.log(req.requestTime)
+    res.download(path.resolve(__dirname, 'static', 'index.html'))
+})
+app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}...`);
+});
